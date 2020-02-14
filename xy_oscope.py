4c0a6f8c16c21ac849a60bf221431b005c4e15e1
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2020-02-15 01:36:43 +0300

    Update xy_oscope.py

--- xy_oscope.py
+++ xy_oscope.py
@@ -14,8 +14,8 @@ def gen(freq, fs, T):
     nsth = signal.sawtooth(val/8)
     nsin2 = np.sin(val/8)
     audio = np.zeros(((fs * T), 2))
-    audio[:, 0] = nsin# - nsin2
-    audio[:, 1] = ncos + np.roll(nsth, 0)
+    audio[:, 0] = nsin # * nsin2
+    audio[:, 1] = ncos + nsth
     #np.roll(note2, 25)
     return audio
 
@@ -34,39 +34,50 @@ def noi(fs, T, mul, arr=None):
     arr[:, 1] += noise*mul[1]
     return arr
 
-freq = 150
-fs = 44100
-T = 4
+def imp(name):
+    a = np.array(read(n)[1])
+    fs = read(n)[0]
+    T = int(a.shape[0]/fs)
+    return a, fs, T
+
+#freq = 150
+#fs = 44100
+#T = 4
+#au = gen(freq, fs, T)
+#aun = noi(fs, T, (0, 0), au)
+#mai = au
+#mu = comp(mai)
+#n = "randmus.wav"
+#write(n, fs, mu)
+
+n = "mush.wav"
+mu, fs, T = imp(n)
+mai = mu.copy()
+pas = 1
 it = 0
-au = gen(freq, fs, T)
-aun = noi(fs, T, (0, 0), au)
 
-mai = au
-
-mu = comp(mai)
-n = "randmus.wav"
-write(n, fs, mu)
-
-pas = 10
-
-evst = fs
-stra = T * fs // evst
+evst = 0#fs//2
+stra = T * fs# // evst
 pd = pyg_draw(2)
 w, h = pd.scr
 mul = 100
+print(mai.shape, np.full(mai.shape[0], mul).shape)
+mai[:, 0] /= np.full(mai.shape[0], mul)
+mai[:, 1] /= np.full(mai.shape[0], mul)
+
 clock = pygame.time.Clock()
 pygame.mixer.music.load(n)
-pygame.mixer.music.set_volume(0.1)
-
-while 1:
+pygame.mixer.music.set_volume(0.5)
+run = True
+while run:
     pygame.mixer.music.play()
         
     for it in range(stra):
-        clock.tick(stra)
-        for i in pygame.event.get():
-            if i.type == pygame.QUIT or i.type == K_ESCAPE:
-                import sys
-                sys.exit()
+        #clock.tick(stra)
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    run = not True
 
         for j in range(0, evst, pas):
             pos = [mai[j*it, 0]*mul, mai[j*it, 1]*mul]
